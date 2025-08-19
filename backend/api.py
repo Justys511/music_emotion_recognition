cd99dc8000f86bed93221d735c036b585b575354
Justys511 <artem.skrypnyk@student.tuke.sk>
2025-08-19 04:36:35 +0200

    update

--- backend/api.py
+++ backend/api.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://music-emotion-recognition.vercel.app/",
+        "https://music-emotion-recognition.vercel.app",
         "http://localhost:3000",
     ],
     allow_credentials=True,
